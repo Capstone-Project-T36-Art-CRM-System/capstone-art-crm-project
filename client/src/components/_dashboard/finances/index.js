05961c3b2ffa6369cb19f4f8a61975e4b66d67fa
oleksii-pedko <61472535+oleksii-pedko@users.noreply.github.com>
2022-03-16 14:27:45 -0400

    Update index.js

--- client/src/components/_dashboard/finances/index.js
+++ client/src/components/_dashboard/finances/index.js
@@ -1,7 +1,4 @@
-export { default as MainActiveStudents } from './MainActiveStudents';
 export { default as MainNewCustomers } from './MainNewCustomers';
 export { default as MainClassesComplited } from './MainClassesComplited';
-export { default as MainLeads } from './MainLeads';
 export { default as MainFinancesGraph } from './MainFinancesGraph';
 export { default as MainSocialsStats } from './MainSocialsStats';
-export { default as MainTasks } from './MainTasks';
